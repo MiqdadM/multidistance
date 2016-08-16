6b390a78d0b46ce2403b58f7a90c18a602729b24
M Miqdad <m.miqdad13@gmail.com>
2016-08-16 07:54:44 +0700

    Update multidistanceTest.php

--- tdd/multidistanceTest.php
+++ tdd/multidistanceTest.php
@@ -20,7 +20,7 @@ class multidistanceTest extends \PHPUnit_Framework_TestCase
         $expected7 = [1.8476846432762];
         $expected8 = [0.37829763262563];
 
-        $test = new multidistance($data1, $data2);
+        $test = new multidistance\multidistance($data1, $data2);
         $test->distance();
         
         $result = $test->getEuclidean();
