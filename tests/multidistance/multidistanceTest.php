5196e679790a7814ee0fe05c12db293784880127
MiqdadM <m.miqdad13@gmail.com>
2016-08-20 10:51:30 +0700

    Release Version 1.0.0 Fix Travis :octocat:

--- tests/multidistance/multidistanceTest.php
+++ tests/multidistance/multidistanceTest.php
@@ -1,6 +1,5 @@
 <?php
-
-use \MiqdadM\Multidistance\multidistance;
+require_once('src/multidistance/multidistance.php');
 
 /**
  *
@@ -21,7 +20,7 @@ class multidistanceTest extends \PHPUnit_Framework_TestCase
         $expected7 = [1.8476846432762];
         $expected8 = [0.37829763262563];
 
-        $test = new multidistance($data1, $data2);
+        $test = new MiqdadM\Multidistance\multidistance($data1, $data2);
         $test->distance();
 
         $result = $test->getEuclidean();
